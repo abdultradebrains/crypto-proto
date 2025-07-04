b1c0d55fd101c9dcc53bed4424e00dd954680e03
Abdul Jaseem <jaseemabdul20@gmail.com>
2025-07-04 14:18:40 +0530

    Commit temp changes

--- options-svelte/svelte.config.js
+++ options-svelte/svelte.config.js
@@ -21,6 +21,9 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter()
 	},
+	alias: {
+		src: '/src',
+	},
 
 	extensions: ['.svelte', '.svx']
 };

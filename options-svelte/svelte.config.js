3983437aa626e7e5cf9384e332af58e9aa30d909
Abdul Jaseem <jaseemabdul20@gmail.com>
2025-07-04 13:29:17 +0530

    Tweak svelte config

--- options-svelte/svelte.config.js
+++ options-svelte/svelte.config.js
@@ -1,4 +1,5 @@
 import { mdsvex } from 'mdsvex';
+import path from "path"
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
@@ -19,6 +20,11 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter()
+	},
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
 	},
 
 	extensions: ['.svelte', '.svx']

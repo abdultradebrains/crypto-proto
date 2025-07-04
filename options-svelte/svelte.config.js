097d978aef094efa564f1616c4e9bc9440fa2d40
Abdul Jaseem <jaseemabdul20@gmail.com>
2025-07-04 13:38:18 +0530

    Fix svelte issues?

--- options-svelte/svelte.config.js
+++ options-svelte/svelte.config.js
@@ -21,11 +21,6 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter()
 	},
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
 
 	extensions: ['.svelte', '.svx']
 };
